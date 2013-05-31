6aa15559ba0b64f1533a9ddb3b917826e195e9e8
Brandon Newman <brandonkylenewman@169-231-82-68.wireless.ucsb.edu>
2013-05-31 15:34:25 -0700

    Added some javadoc, added a clean task to build.xml. Got rid of any mention of serialization; tougher than expected

--- src/edu/ucsb/cs56/projects/utilities/todo/Task.java
+++ src/edu/ucsb/cs56/projects/utilities/todo/Task.java
@@ -4,8 +4,15 @@ import java.util.Calendar;
 import java.util.ArrayList;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+import java.io.Serializable;
 
-public class Task {
+/**
+	A single task in a todo list
+
+	@author Brandon Newman
+	@version todo project for CS56 S13
+*/
+public class Task implements Serializable{
 
 	private String taskName;
 	private Calendar dueDate;
@@ -13,6 +20,15 @@ public class Task {
 	//private int numSubtasks;
 	//private ArrayList<Task> subTasks;
 
+	/**
+	Constructor for a single task
+	@parem taskName the name of the new task
+	@parem year the year in which the due date is
+	@parem month the month in which the due date is
+	@parem day the day in which the due date is
+	@parem hour the hour in which the due date is
+	@parem min the minute at which the due date is
+	*/
 	public Task(String taskName, int year, int month, int day, int hour, int min)
 	{
 		this.taskName = taskName;
@@ -20,28 +36,55 @@ public class Task {
 		this.completed = false;
 	}
 
+	/**
+	Returns the name of the task
+	*/
 	public String getName()
 	{
 		return this.taskName;
 	}
 
+	/**
+	Returns the due date in the format of "month/day/year hour:minute" ex. "05/21/13 04:00"
+	*/
 	public String getDueDate()
 	{
 		SimpleDateFormat date_format = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 		return date_format.format(this.dueDate.getTime());
 	}
 
+	/**
+	Marks a task as completed
+	*/
+	public void markCompleted()
+	{
+		this.completed = true;
+	}
+
+	/**
+	Returns the completion status of a particular task
+	*/
+	public boolean getCompleted()
+	{
+		return this.completed;
+	}
+
+	/**
+	Overwriting the toString method. Prints in the format of:
+	"[ ] task name month/day/year hour:minute"
+	example:
+	"[ ] uncompleted task 05/22/13 04:22"
+	*/
 	public String toString()
 	{
 		String result = "";
 
-		if (completed == false)
-		{
+		if (this.completed == false)
 			result = "[ ] " + this.getName() + " " + this.getDueDate();
-		}
+		else
+			result = "[x] " + this.getName() + " " + this.getDueDate();
+
 		return result;
 	}
-
-	//TODO1: Constuctor
 
 }
--- src/edu/ucsb/cs56/projects/utilities/todo/Todo.java
+++ src/edu/ucsb/cs56/projects/utilities/todo/Todo.java
@@ -8,13 +8,7 @@ import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.io.*;
 
-
-public class Todo{
-
-	public void addTask(String name, String dueDate)
-	{
-		//Will create a new Task and stick it in the Arraylist
-	}
+public class Todo implements Serializable {
 
 	public static void main(String[] args)
 	{
@@ -24,6 +18,7 @@ public class Todo{
 
 		while(end)
 		{
+			System.out.println("");
 			System.out.println("--------TODO--------");
 			for (int i = 0; i < numTasks; i++)
 				System.out.println(tasks.get(i).toString());
@@ -32,7 +27,7 @@ public class Todo{
 			System.out.println("--------------------");
 
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("add, delete, or mark a task");
+			System.out.println("add, delete, or mark a task, or exit.");
 			String input = scanner.nextLine();
 
 			if (input.equals("add"))
@@ -69,20 +64,60 @@ public class Todo{
 			}
 			else if (input.equals("delete"))
 			{
+				System.out.println("What's the task would you like to delete completed?");
+				String taskName = scanner.nextLine();
+
+				int deleteIndex = -1;
+
+				for (int i = 0; i < numTasks; i++)
+				{
+					if (tasks.get(i).getName().equals(taskName))
+					{
+						deleteIndex = i;
+						break;
+					}
+				}
+
+				if (deleteIndex != -1)
+				{
+					tasks.remove(deleteIndex);
+					numTasks--;
+				}
+				else
+					System.out.println("Aww shucks. Not a valid task name.");
 
 			}
 			else if (input.equals("mark"))
 			{
+				System.out.println("What's the task would you like to mark completed?");
+				String taskName = scanner.nextLine();
 
+				int markIndex = -1;
+
+				for (int i = 0; i < numTasks; i++)
+				{
+					if (tasks.get(i).getName().equals(taskName))
+					{
+						markIndex = i;
+						break;
+					}
+				}
+
+				if (markIndex != -1)
+				{
+					if (tasks.get(markIndex).getCompleted() == false)
+						tasks.get(markIndex).markCompleted();
+					else
+						System.out.println("Task already marked completed");
+				}
 			}
 			else if (input.equals("exit"))
 			{
-
+				System.out.println("Would you like to save?");
+				String exitInput = scanner.nextLine();
 			}
 			else
 				System.out.println("Not a valid input :(");		
-			//wait for command
-			//add, delete, mark
 
 		}
 
