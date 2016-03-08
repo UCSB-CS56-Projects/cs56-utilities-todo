bd5575164d7a26c63f72bb7e75e599a72dfef18b
William Stevenson <wjstevenson@umail.ucsb.edu>
2016-03-08 12:00:14 -0800

    Lab 09: Fixed many code bugs

--- src/edu/ucsb/cs56/projects/utilities/todo/Todo.java
+++ src/edu/ucsb/cs56/projects/utilities/todo/Todo.java
@@ -133,55 +133,57 @@ public class Todo implements Serializable {
 		System.err.println("Caught ClassNotFoundException: " + e.getMessage());
 	    }
 
-	// Add code for List compoenents (need to be added before tasks)
-	for(int i=0; i < taskList.getLists().size(); i++) {
-	    String listInfo = taskList.getLists().get(i).toString();
-
-	    JRadioButton listCheckTemp = new JRadioButton();
-	    JButton listDeleteTemp = new JButton("X");
-	    JButton listEditTemp = new JButton("Edit");
-
-	    listCheckTemp.addActionListener(new radioButtonListener(taskList.getLists().get(i)));
-	    listDeleteTemp.addActionListener(new DeleteListListener(taskList.getLists().get(i)));
-	    listEditTemp.addActionListener(new EditListListener(taskList.getLists().get(i)));
-
-	    taskList.getLists().get(i).setEdit(listEditTemp);
-	    taskList.getLists().get(i).setCheck(listCheckTemp);
-	    taskList.getLists().get(i).setDelete(listDeleteTemp);
-
-	    taskList.getListGroup().add(taskList.getLists().get(i).getCheck());
-	}
-
-	this.displayLists();
-
-	
-	//adds mainPanel components to their respective ArrayLists
-	for (int i=0; i < taskList.getCurrentList().getTasks().size(); i++) {
-
-	    //checks for completion and removes "[]" or "[x]"
-	    String taskInfo = taskList.getCurrentList().getTasks().get(i).toString();
-	    if (taskInfo.indexOf("x") == 1) {
-		taskInfo = taskInfo.substring(4);
+	// Add code for List compoenents (need to be added before tasks)'
+	if(!taskList.getLists().isEmpty()) {
+	    for(int i=0; i < taskList.getLists().size(); i++) {
+		String listInfo = taskList.getLists().get(i).toString();
+		
+		JRadioButton listCheckTemp = new JRadioButton();
+		JButton listDeleteTemp = new JButton("X");
+		JButton listEditTemp = new JButton("Edit");
+		
+		listCheckTemp.addActionListener(new radioButtonListener(taskList.getLists().get(i)));
+		listDeleteTemp.addActionListener(new DeleteListListener(taskList.getLists().get(i)));
+		listEditTemp.addActionListener(new EditListListener(taskList.getLists().get(i)));
+		
+		taskList.getLists().get(i).setEdit(listEditTemp);
+		taskList.getLists().get(i).setCheck(listCheckTemp);
+		taskList.getLists().get(i).setDelete(listDeleteTemp);
+		
+		taskList.getListGroup().add(taskList.getLists().get(i).getCheck());
 	    }
-	    else {
-		taskInfo = taskInfo.substring(3);
-	    }
-
-	    JCheckBox checkTemp = new JCheckBox();
-	    JButton buttonTemp = new JButton("Delete");
-	    JButton editTemp = new JButton("Edit");
-
-	    buttonTemp.addActionListener(new DeleteListener(taskList.getCurrentList().getTasks().get(i)));
-	    editTemp.addActionListener(new EditListener(taskList.getCurrentList().getTasks().get(i)));
 	    
-
-	    taskList.getCurrentList().getTasks().get(i).setEdit(editTemp);
-	    taskList.getCurrentList().getTasks().get(i).setCheck(checkTemp);
-	    taskList.getCurrentList().getTasks().get(i).setDelete(buttonTemp);
-	}
+	    this.displayLists();
+	    
+	    
+	    //adds mainPanel components to their respective ArrayLists
+	    for (int i=0; i < taskList.getCurrentList().getTasks().size(); i++) {
+		
+		//checks for completion and removes "[]" or "[x]"
+		String taskInfo = taskList.getCurrentList().getTasks().get(i).toString();
+		if (taskInfo.indexOf("x") == 1) {
+		    taskInfo = taskInfo.substring(4);
+		}
+		else {
+		    taskInfo = taskInfo.substring(3);
+		}
+		
+		JCheckBox checkTemp = new JCheckBox();
+		JButton buttonTemp = new JButton("Delete");
+		JButton editTemp = new JButton("Edit");
+		
+		buttonTemp.addActionListener(new DeleteListener(taskList.getCurrentList().getTasks().get(i)));
+		editTemp.addActionListener(new EditListener(taskList.getCurrentList().getTasks().get(i)));
+		
+		
+		taskList.getCurrentList().getTasks().get(i).setEdit(editTemp);
+		taskList.getCurrentList().getTasks().get(i).setCheck(checkTemp);
+		taskList.getCurrentList().getTasks().get(i).setDelete(buttonTemp);
+	    }
 	
 	//displays the taskPanel components
-        this.displayTasks();
+	    this.displayTasks();
+	}
 
 	JButton sortPriorityButton = new JButton("SORT BY PRIORITY");
        	JButton sortNameButton = new JButton("SORT BY NAME");
@@ -308,16 +310,18 @@ public class Todo implements Serializable {
 
     public class sortPriorityListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    sorted = true;
-	    taskList.getCurrentList().getSortedTasks().clear();
-	    taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
-	    Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByPriority());
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
+	    if(!taskList.getLists().isEmpty()) {
+		sorted = true;
+		taskList.getCurrentList().getSortedTasks().clear();
+		taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
+		Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByPriority());
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
 	    }
 	}
     }
@@ -325,48 +329,55 @@ public class Todo implements Serializable {
 
     public class sortNameListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    sorted = true;
-	    taskList.getCurrentList().getSortedTasks().clear();
-	    taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
-	    Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByName());
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
+	    if(!taskList.getLists().isEmpty()) {
+		sorted = true;
+		taskList.getCurrentList().getSortedTasks().clear();
+		taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
+		Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByName());
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
 	    }
 	}
     }
+    
     public class sortCompletionListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    sorted = true;
-	    taskList.getCurrentList().getSortedTasks().clear();
-	    taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
-	    Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByCompleted());
-	    
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
+	    if(!taskList.getLists().isEmpty()) {
+		sorted = true;
+		taskList.getCurrentList().getSortedTasks().clear();
+		taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
+		Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByCompleted());
+		
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
 	    }
 	}
-
     }
+    
     public class sortDateListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    sorted = true;
-	    taskList.getCurrentList().getSortedTasks().clear();
-	    taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
-	    Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByDate());
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
+	    if(!taskList.getLists().isEmpty()) {
+		sorted = true;
+		taskList.getCurrentList().getSortedTasks().clear();
+		taskList.getCurrentList().updateSortedList(taskList.getCurrentList().getTasks(), taskList.getCurrentList().getSortedTasks());
+		Collections.sort(taskList.getCurrentList().getSortedTasks(), taskList.getCurrentList().compareByDate());
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
 	    }
 	}
     }
@@ -375,10 +386,12 @@ public class Todo implements Serializable {
 	public void actionPerformed(ActionEvent ev) {
 	    mainPanel.removeAll();
 	    mainPanel.repaint();
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
+	    if(!taskList.getLists().isEmpty()) {
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
 	    }
 	}
     }
@@ -389,17 +402,47 @@ public class Todo implements Serializable {
 	    this.myList = myList;
 	}
 	public void actionPerformed(ActionEvent ev) {
-	    //Delete all the tasks in that List
+	    // Delete all the tasks in that List
 	    // Then remove the list from taskList
+	    boolean current = false;
+	    if(taskList.getCurrentList() == this.myList) {
+		current = true;
+	    }
+
+	    // Find where the list to be deleted is in the ArrayList of Lists
+	    // Then go through the tasks in that List and delete all the tasks
 	    int j = taskList.getLists().indexOf(this.myList);
 	    for(int i = 0; i < taskList.getLists().get(j).getTasks().size(); i++) {
 		taskList.getLists().get(j).getTasks().remove(taskList.getLists().get(j).getTasks().get(i));
-		taskList.getLists().get(j).getSortedTasks().remove(taskList.getLists().get(j).getSortedTasks().get(i));
+		//taskList.getLists().get(j).getSortedTasks().remove(taskList.getLists().get(j).getSortedTasks().get(i));
 	    }
+
 	    taskList.getLists().remove(this.myList);
+
+	    // If list being deleted was the currentList and new size isn't 0
+	    // Make the first item in the list the currentList
+	    if(!taskList.getLists().isEmpty()) {
+		if(current) {
+		    taskList.setCurrentList(taskList.getLists().get(0));
+		}
+	    } else {
+		taskList.setCurrentList(null);
+	    }
+	    
 	    leftPanel.removeAll();
 	    leftPanel.repaint();
 	    displayLists();
+	    mainPanel.removeAll();
+	    mainPanel.repaint();
+
+	    // If new size is 0, don't call displayTasks(), because currentList is null
+	    if(!taskList.getLists().isEmpty()) {
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
+	    }
 	}
     }
 	    
@@ -441,6 +484,12 @@ public class Todo implements Serializable {
 	    taskList.getLists().get(i).setCheck(listCheckTemp);
 	    taskList.getLists().get(i).setDelete(listDeleteTemp);
 	    taskList.getListGroup().add(taskList.getLists().get(i).getCheck());
+
+	    // If newly added List is only List in TodoList, make it current List
+	    if(taskList.getLists().size() == 1) {
+		taskList.setCurrentList(taskList.getLists().get(0));
+	    }
+	    
 	    addListField.setText("");
 	    leftPanel.removeAll();
 	    leftPanel.repaint();
@@ -453,46 +502,49 @@ public class Todo implements Serializable {
 	    
     public class AddListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    sorted = false;
-	    int redInt = (Integer) red.getValue();
-	    int greenInt = (Integer) green.getValue();
-	    int blueInt = (Integer) blue.getValue();
-	    Color c = new Color(redInt, greenInt, blueInt);
-	    //taskList.updateSortedList(taskList.getSortedTasks(), taskList.getTasks());
-	    if(one.isSelected() == true) {
-		taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),1,c));
-	    } else if(two.isSelected() == true) {
-		taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),2,c));
-	    } else if(three.isSelected() == true) {
-		taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),3,c));
-	    } else {
-		taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),0,c));
+	    // There must be a list in order to make a task
+	    if(!taskList.getLists().isEmpty()) {
+		sorted = false;
+		int redInt = (Integer) red.getValue();
+		int greenInt = (Integer) green.getValue();
+		int blueInt = (Integer) blue.getValue();
+		Color c = new Color(redInt, greenInt, blueInt);
+		//taskList.updateSortedList(taskList.getSortedTasks(), taskList.getTasks());
+		if(one.isSelected() == true) {
+		    taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),1,c));
+		} else if(two.isSelected() == true) {
+		    taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),2,c));
+		} else if(three.isSelected() == true) {
+		    taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),3,c));
+		} else {
+		    taskList.getCurrentList().addTask(taskList.getCurrentList().makeTask(addField.getText(),0,c));
+		}
+		
+		//add new checkbox and buttons
+		JCheckBox checkTemp = new JCheckBox();
+		JButton buttonTemp = new JButton("Delete");
+		int i = taskList.getCurrentList().getTasks().size()-1;
+		buttonTemp.addActionListener(new DeleteListener(taskList.getCurrentList().getTasks().get(i)));
+		JButton editTemp = new JButton("Edit");
+		editTemp.addActionListener(new EditListener(taskList.getCurrentList().getTasks().get(i)));
+		taskList.getCurrentList().getTasks().get(i).setEdit(editTemp);
+		taskList.getCurrentList().getTasks().get(i).setCheck(checkTemp);
+		taskList.getCurrentList().getTasks().get(i).setDelete(buttonTemp);
+		addField.setText("");
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		
+		if(hidden == true) {
+		    displayComTasks();
+		} else {
+		    displayTasks();
+		}
+		
+		cbg.clearSelection();
+		red.setValue(0);
+		green.setValue(0);
+		blue.setValue(0);
 	    }
-	    
-	    //add new checkbox and buttons
-	    JCheckBox checkTemp = new JCheckBox();
-	    JButton buttonTemp = new JButton("Delete");
-	    int i = taskList.getCurrentList().getTasks().size()-1;
-	    buttonTemp.addActionListener(new DeleteListener(taskList.getCurrentList().getTasks().get(i)));
-	    JButton editTemp = new JButton("Edit");
-	    editTemp.addActionListener(new EditListener(taskList.getCurrentList().getTasks().get(i)));
-	    taskList.getCurrentList().getTasks().get(i).setEdit(editTemp);
-	    taskList.getCurrentList().getTasks().get(i).setCheck(checkTemp);
-	    taskList.getCurrentList().getTasks().get(i).setDelete(buttonTemp);
-	    addField.setText("");
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-
-	    if(hidden == true) {
-		displayComTasks();
-	    } else {
-		displayTasks();
-	    }
-	    
-	    cbg.clearSelection();
-	    red.setValue(0);
-	    green.setValue(0);
-	    blue.setValue(0);
 	}
     }
 
@@ -734,44 +786,49 @@ public class Todo implements Serializable {
 
     public class toggleCompletedListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
-	    if(hidden == true) {
-		mainPanel.removeAll();
-		mainPanel.repaint();
-		displayTasks();
-		hidden = false;
-		timer.stop();
-	    } else {
-		mainPanel.removeAll();
-		mainPanel.repaint();
-		displayComTasks();
-		hidden = true;
-		timer.start();
+	    if(!taskList.getLists().isEmpty()) {
+		if(hidden == true) {
+		    mainPanel.removeAll();
+		    mainPanel.repaint();
+		    displayTasks();
+		    hidden = false;
+		    timer.stop();
+		} else {
+		    mainPanel.removeAll();
+		    mainPanel.repaint();
+		    displayComTasks();
+		    hidden = true;
+		    timer.start();
+		}
 	    }
 	}
     }
     
     public class deleteCompletedListener implements ActionListener{
 	public void actionPerformed(ActionEvent ev){
-	    if(sorted==true){
-       		for(int i = 0; i<taskList.getCurrentList().getSortedTasks().size();i++){
-		    if(taskList.getCurrentList().getSortedTasks().get(i).getCheck().isSelected()==true){
-			taskList.getCurrentList().getTasks().remove(taskList.getCurrentList().getSortedTasks().get(i));
-			taskList.getCurrentList().getSortedTasks().remove(taskList.getCurrentList().getSortedTasks().get(i));
-			i = 0;
+	    if(!taskList.getLists().isEmpty()) {
+		if(sorted==true){
+		    for(int i = 0; i<taskList.getCurrentList().getSortedTasks().size();i++){
+			if(taskList.getCurrentList().getSortedTasks().get(i).getCheck().isSelected()==true){
+			    taskList.getCurrentList().getTasks().remove(taskList.getCurrentList().getSortedTasks().get(i));
+			    taskList.getCurrentList().getSortedTasks().remove(taskList.getCurrentList().getSortedTasks().get(i));
+			    i = 0;
+			}
 		    }
 		}
-	    }
-	    else{
-		for(int i = 0; i< taskList.getCurrentList().getTasks().size();i++){
-		    if(taskList.getCurrentList().getTasks().get(i).getCheck().isSelected()==true){
-			taskList.getCurrentList().getTasks().remove(taskList.getCurrentList().getTasks().get(i));
-			i = 0;
+		else{
+		    for(int i = 0; i< taskList.getCurrentList().getTasks().size();i++){
+			if(taskList.getCurrentList().getTasks().get(i).getCheck().isSelected()==true){
+			    taskList.getCurrentList().getTasks().remove(taskList.getCurrentList().getTasks().get(i));
+			    i = 0;
+			}
 		    }
 		}
+
+		mainPanel.removeAll();
+		mainPanel.repaint();
+		displayTasks();
 	    }
-	    mainPanel.removeAll();
-	    mainPanel.repaint();
-	    displayTasks();
 	}
     }
 
